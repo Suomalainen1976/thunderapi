d545bf57dbef3903fde64edc4a03507d27dccce6
Michel Nguyen <modzongaming@gmail.com>
2018-01-20 15:18:01 +0100

    update readme examples, implement caching
    Signed-off-by: Michel Nguyen <modzongaming@gmail.com>

--- README.md
+++ README.md
@@ -64,10 +64,16 @@ npm install devdutchy/thunderapi
 
 ```js
 const { ThunderAPI } = require("thunderapi");
+// Replace 'useragent' with either your in-game name, or
+// with something else which confirms your identity.
 const thunderAPI = new ThunderAPI("useragent");
 
+// The player can be any in-game player. In this example,
+// we'll get the profile of the player Abinavski, and log
+// the title he set.
 thunderAPI.getPlayer("Abinavski")
   .then(data => console.log("Title:", data.title))
+  // If an error occurred, we catch it here!
   .catch(err => console.error("Oh no, an error occurred!\n", err));
 ```
 
--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@ class ThunderApi {
   /**
    * Creates a new ThunderAPI class
    * @param {string} userAgent The useragent to use
+   * @param {number} cacheSweepInterval The timeout in milliseconds to sweep the cache, default being 180000 milliseconds
    */
-  constructor(userAgent) {
+  constructor(userAgent, cacheSweepInterval = 180000) {
     /**
      * The User Agent
      * @type {string}
@@ -24,11 +25,34 @@ class ThunderApi {
      * @readonly
      */
     this.requestManager = new RequestManager(this.USER_AGENT, Package.version);
+    /**
+     * A map of cached profiles/squadrons.
+     * Cache will be sweeped after the time passed
+     * @type {Map<string,Profile|Squadron>}
+     * @private
+     */
+    this.cache = new Map();
+    /**
+     * The time cache should be sweeped, in milliseconds
+     * @type {number}
+     */
+    this.cacheSweepInterval = cacheSweepInterval;
+    /**
+     * All the intervals set
+     * @type {Array}
+     * @private
+     */
+    this._intervals = [];
+
+    const interval = setInterval(this.cache.clear, this.cacheSweepInterval);
+    this._intervals.push(interval);
   }
 
   /**
    * Get a player's profile
    * @param {string} player The profile of the player to fetch
+   * @param {boolean?} getFromCache If it should get the player from the cache. Defaults to true
+   * @param {boolean?} shouldCache If it should cache the profile. Defaults to true
    * @return {Promise<Profile>}
    * @example
    * // The following example gets the profile of the player
@@ -41,11 +65,16 @@ class ThunderApi {
    *   })
    *   .catch(err => console.error("Oh no, an error occurred!", err));
    */
-  getPlayer(player) {
+  getPlayer(player, getFromCache = true, shouldCache = true) {
     return new Promise(async (resolve, reject) => {
+      if (this.cache.has(player) && getFromCache) {
+        const profile = this.cache.get(player);
+        resolve(profile);
+      }
       const data = await this.requestManager.get("profile", player);
       if (data.error) reject(new Error(data.error));
       const profile = new Profile(data);
+      if (shouldCache) this.cache.set(player, profile);
       resolve(profile);
     });
   }
@@ -71,6 +100,8 @@ class ThunderApi {
    * e.g. "35th Gopnik nation battle group" instead of
    * "GOPNK".</note>
    * @param {string} name The **full** name of the squadron
+   * @param {boolean?} getFromCache If it should get the squadron from the cache, if cached. Defaults to true
+   * @param {boolean?} shouldCache If it should cache the squadron. Defaults to true
    * @return {Promise<Squadron>}
    * @example
    * // The following example gets info about
@@ -84,11 +115,16 @@ class ThunderApi {
    *   })
    *   .catch(err => console.error("Oh no, an error occurred!", err));
    */
-  getSquadron(name) {
+  getSquadron(name, getFromCache = true, shouldCache = true) {
     return new Promise(async (resolve, reject) => {
+      if (this.cache.has(name) && getFromCache) {
+        const squadron = this.cache.get(name);
+        resolve(squadron);
+      }
       const data = await this.requestManager.get("squadron", name);
       if (data.error) reject(new Error(data.error));
       const squadron = new Squadron(data);
+      if (shouldCache) this.cache.set(name, squadron);
       resolve(squadron);
     });
   }
@@ -106,13 +142,26 @@ class ThunderApi {
       resolve(data);
     });
   }
+
+  /**
+   * Clears all the intervals
+   * @private
+   */
+  _clearIntervals() {
+    for (const interval of this._intervals) {
+      clearInterval(interval);
+      this._intervals.splice(this._intervals.indexOf(interval), 1);
+    }
+  }
 }
 
 module.exports = {
+  // Core
   ThunderAPI: ThunderApi,
   RequestManager,
   version: Package.version,
 
+  // Structures
   Profile: require("./structures/Profile"),
   Squadron: require("./structures/squadron")
 };
